f30bde3aa8f88ae78315593aa19df777c8e76570
Ptr <petarman96@abv.bg>
2020-02-19 18:09:58 +0200

    added new method that generates four digit number with no repetitive numbers both to the CowsAndBulls and CowsAndBullsTest classes

--- src/CowsAndBulls.java
+++ src/CowsAndBulls.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class CowsAndBulls {
@@ -117,5 +118,37 @@ public class CowsAndBulls {
 
         System.out.println("There are " + cowsCount + " cows");
 
+    }
+
+    //method that generates four digit number with no repetitive digits
+    public static int GeneratesRandomNumber() {
+        Random rand = new Random();
+        int digitOne = rand.nextInt(9) + 1;
+        int digitTwo = 0;
+        int digitThree = 0;
+        int digitFour = 0;
+
+        do {
+            digitTwo = rand.nextInt(9) + 1;
+        } while (digitTwo == digitOne);
+
+        do {
+            digitThree = rand.nextInt(9) + 1;
+        } while (digitThree == digitOne || digitThree == digitTwo);
+
+        do {
+            digitFour = rand.nextInt(9) + 1;
+        } while (digitFour == digitOne || digitFour == digitTwo || digitFour == digitThree);
+
+        String digitOneStr = Integer.toString(digitOne);
+        String digitTwoStr = Integer.toString(digitTwo);
+        String digitThreeStr = Integer.toString(digitThree);
+        String digitFourStr = Integer.toString(digitFour);
+
+        String digitConcat = digitOneStr + digitTwoStr + digitThreeStr + digitFourStr;
+
+        int fourDigitNumber = Integer.parseInt(digitConcat);
+
+        return fourDigitNumber;
     }
 }
