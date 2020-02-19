233dd6d7d03c9cf7206db7301a824372e1914c82
Ptr <petarman96@abv.bg>
2020-02-19 16:43:55 +0200

    Added the code from test class to project class

--- src/CowsAndBulls.java
+++ src/CowsAndBulls.java
@@ -1,15 +1,121 @@
+import java.util.Scanner;
+
 public class CowsAndBulls {
     public static void main(String[] args) {
-        int[] numberToGuess = {3,2,2,6};
-        int[] userInput = {3,2,4,5};
-        int bullCounter = 0;
+        Scanner input = new Scanner(System.in);
+        int numberToGuess = 5432;
+        int userInput = 3421;
 
-        for(int i = 0; i < numberToGuess.length;i++){
-            if(numberToGuess[i] == userInput[i]){
-                bullCounter++;
-            }
+        int bullsCount = 0;
+        int cowsCount = 0;
 
+        //taking each digit of the guess number
+        int tempGuessNumber = numberToGuess;
+
+        int guessDigitOne = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
+        System.out.println(guessDigitOne);
+
+        int guessDigitTwo = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
+        System.out.println(guessDigitTwo);
+
+        int guessDigitThree = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
+        System.out.println(guessDigitThree);
+
+        int guessDigitFour = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
+        System.out.println(guessDigitFour);
+
+        //taking each digit of the user input number
+        int tempUserInputNumber = userInput;
+        int userInputDigitOne = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
+        System.out.println(userInputDigitOne);
+
+        int userInputDigitTwo = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
+        System.out.println(userInputDigitTwo);
+
+        int userInputDigitThree = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
+        System.out.println(userInputDigitThree);
+
+        int userInputDigitFour = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
+        System.out.println(userInputDigitFour);
+
+        //adding to bull counter
+        if (userInputDigitFour == guessDigitFour) {
+            bullsCount++;
         }
-        System.out.println("There are " + bullCounter + " bulls");
+
+        if (userInputDigitThree == guessDigitThree) {
+            bullsCount++;
+        }
+
+        if (userInputDigitTwo == guessDigitTwo) {
+            bullsCount++;
+        }
+
+        if (userInputDigitOne == guessDigitOne) {
+            bullsCount++;
+        }
+
+        System.out.println("There are " + bullsCount + " bulls in your number");
+        //adding to cow counter
+
+        //checking if the 4th digit is matching with any of the rest and adds to cows' counter
+        if (userInputDigitFour == guessDigitThree) {
+            cowsCount++;
+        }
+
+        if (userInputDigitFour == guessDigitTwo) {
+            cowsCount++;
+        }
+
+        if (userInputDigitFour == guessDigitOne) {
+            cowsCount++;
+        }
+        //checking if the 3th digit is matching with any of the rest and adds to cows' counter
+        if (userInputDigitThree == guessDigitFour) {
+            cowsCount++;
+        }
+
+        if (userInputDigitThree == guessDigitTwo) {
+            cowsCount++;
+        }
+
+        if (userInputDigitThree == guessDigitOne) {
+            cowsCount++;
+        }
+        //checking if the 2nd digit is matching with any of the rest and adds to cows' counter
+        if (userInputDigitTwo == guessDigitFour) {
+            cowsCount++;
+        }
+
+        if (userInputDigitTwo == guessDigitThree) {
+            cowsCount++;
+        }
+
+        if (userInputDigitTwo == guessDigitOne) {
+            cowsCount++;
+        }
+        //checking if the 1st digit is matching with any of the rest and adds to cows' counter
+        if (userInputDigitOne == guessDigitFour) {
+            cowsCount++;
+        }
+
+        if (userInputDigitOne == guessDigitThree) {
+            cowsCount++;
+        }
+
+        if (userInputDigitOne == guessDigitTwo) {
+            cowsCount++;
+        }
+
+        System.out.println("There are " + cowsCount + " cows");
+
     }
 }
--- src/CowsAndBullsTest.java
+++ src/CowsAndBullsTest.java
@@ -12,54 +12,54 @@ public class CowsAndBullsTest {
         //taking each digit of the guess number
         int tempGuessNumber = numberToGuess;
 
-        int guessDigitOne = tempGuessNumber%10;
-        tempGuessNumber = tempGuessNumber/10;
+        int guessDigitOne = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
         System.out.println(guessDigitOne);
 
-        int guessDigitTwo = tempGuessNumber%10;
-        tempGuessNumber = tempGuessNumber/10;
+        int guessDigitTwo = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
         System.out.println(guessDigitTwo);
 
-        int guessDigitThree = tempGuessNumber%10;
-        tempGuessNumber = tempGuessNumber/10;
+        int guessDigitThree = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
         System.out.println(guessDigitThree);
 
-        int guessDigitFour = tempGuessNumber%10;
-        tempGuessNumber = tempGuessNumber/10;
+        int guessDigitFour = tempGuessNumber % 10;
+        tempGuessNumber = tempGuessNumber / 10;
         System.out.println(guessDigitFour);
 
         //taking each digit of the user input number
         int tempUserInputNumber = userInput;
-        int userInputDigitOne = tempUserInputNumber%10;
-        tempUserInputNumber = tempUserInputNumber/10;
+        int userInputDigitOne = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
         System.out.println(userInputDigitOne);
 
-        int userInputDigitTwo = tempUserInputNumber%10;
-        tempUserInputNumber = tempUserInputNumber/10;
+        int userInputDigitTwo = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
         System.out.println(userInputDigitTwo);
 
-        int userInputDigitThree = tempUserInputNumber%10;
-        tempUserInputNumber = tempUserInputNumber/10;
+        int userInputDigitThree = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
         System.out.println(userInputDigitThree);
 
-        int userInputDigitFour = tempUserInputNumber%10;
-        tempUserInputNumber = tempUserInputNumber/10;
+        int userInputDigitFour = tempUserInputNumber % 10;
+        tempUserInputNumber = tempUserInputNumber / 10;
         System.out.println(userInputDigitFour);
 
         //adding to bull counter
-        if(userInputDigitFour == guessDigitFour){
+        if (userInputDigitFour == guessDigitFour) {
             bullsCount++;
         }
 
-        if(userInputDigitThree == guessDigitThree){
+        if (userInputDigitThree == guessDigitThree) {
             bullsCount++;
         }
 
-        if(userInputDigitTwo == guessDigitTwo) {
+        if (userInputDigitTwo == guessDigitTwo) {
             bullsCount++;
         }
 
-        if(userInputDigitOne == guessDigitOne) {
+        if (userInputDigitOne == guessDigitOne) {
             bullsCount++;
         }
 
@@ -67,59 +67,55 @@ public class CowsAndBullsTest {
         //adding to cow counter
 
         //checking if the 4th digit is matching with any of the rest and adds to cows' counter
-        if(userInputDigitFour == guessDigitThree){
+        if (userInputDigitFour == guessDigitThree) {
             cowsCount++;
         }
 
-        if(userInputDigitFour == guessDigitTwo){
+        if (userInputDigitFour == guessDigitTwo) {
             cowsCount++;
         }
 
-        if(userInputDigitFour == guessDigitOne){
+        if (userInputDigitFour == guessDigitOne) {
             cowsCount++;
         }
         //checking if the 3th digit is matching with any of the rest and adds to cows' counter
-        if(userInputDigitThree == guessDigitFour){
+        if (userInputDigitThree == guessDigitFour) {
             cowsCount++;
         }
 
-        if(userInputDigitThree == guessDigitTwo){
+        if (userInputDigitThree == guessDigitTwo) {
             cowsCount++;
         }
 
-        if(userInputDigitThree == guessDigitOne){
+        if (userInputDigitThree == guessDigitOne) {
             cowsCount++;
         }
         //checking if the 2nd digit is matching with any of the rest and adds to cows' counter
-        if(userInputDigitTwo == guessDigitFour){
+        if (userInputDigitTwo == guessDigitFour) {
             cowsCount++;
         }
 
-        if(userInputDigitTwo == guessDigitThree){
+        if (userInputDigitTwo == guessDigitThree) {
             cowsCount++;
         }
 
-        if(userInputDigitTwo == guessDigitOne){
+        if (userInputDigitTwo == guessDigitOne) {
             cowsCount++;
         }
         //checking if the 1st digit is matching with any of the rest and adds to cows' counter
-        if(userInputDigitOne == guessDigitFour){
+        if (userInputDigitOne == guessDigitFour) {
             cowsCount++;
         }
 
-        if(userInputDigitOne == guessDigitThree){
+        if (userInputDigitOne == guessDigitThree) {
             cowsCount++;
         }
 
-        if(userInputDigitOne == guessDigitTwo){
+        if (userInputDigitOne == guessDigitTwo) {
             cowsCount++;
         }
 
         System.out.println("There are " + cowsCount + " cows");
-
-
-
-
 
     }
 }
