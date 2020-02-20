deb3afe4159ec2d5d20401dae3494cb0e58ec29e
Ptr <petarman96@abv.bg>
2020-02-20 13:39:42 +0200

    Added a boolean and while loop so that it keeps asking the user for new number until they guess it. Edited the method name generateRandomNumber(); since it was starting with a capital letter and edited the method so that it can generate zeros for the 2nd 3rd and 4th number excluding the first one

--- src/CowsAndBullsTest.java
+++ src/CowsAndBullsTest.java
@@ -1,12 +1,137 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class CowsAndBullsTest {
     public static void main(String[] args) {
-        System.out.println(GeneratesRandomNumber());
+        Scanner input = new Scanner(System.in);
+        int numberToGuess = generatesRandomNumber();
+
+
+        int bullsCount = 0;
+        int cowsCount = 0;
+
+        boolean isGameRunning = true;
+
+        while (isGameRunning) {
+            System.out.print(numberToGuess + " Enter 4 digit number with different digits: ");
+            int userInput = input.nextInt();
+
+
+            //taking each digit of the guess number
+            int tempGuessNumber = numberToGuess;
+
+            int guessDigitOne = tempGuessNumber % 10;
+            tempGuessNumber = tempGuessNumber / 10;
+
+            int guessDigitTwo = tempGuessNumber % 10;
+            tempGuessNumber = tempGuessNumber / 10;
+
+            int guessDigitThree = tempGuessNumber % 10;
+            tempGuessNumber = tempGuessNumber / 10;
+
+            int guessDigitFour = tempGuessNumber % 10;
+            tempGuessNumber = tempGuessNumber / 10;
+
+
+            //taking each digit of the user input number
+            int tempUserInputNumber = userInput;
+
+            int userInputDigitOne = tempUserInputNumber % 10;
+            tempUserInputNumber = tempUserInputNumber / 10;
+
+            int userInputDigitTwo = tempUserInputNumber % 10;
+            tempUserInputNumber = tempUserInputNumber / 10;
+
+            int userInputDigitThree = tempUserInputNumber % 10;
+            tempUserInputNumber = tempUserInputNumber / 10;
+
+            int userInputDigitFour = tempUserInputNumber % 10;
+            tempUserInputNumber = tempUserInputNumber / 10;
+
+
+            //adding to bull counter
+            if (userInputDigitFour == guessDigitFour) {
+                bullsCount++;
+            }
+
+            if (userInputDigitThree == guessDigitThree) {
+                bullsCount++;
+            }
+
+            if (userInputDigitTwo == guessDigitTwo) {
+                bullsCount++;
+            }
+
+            if (userInputDigitOne == guessDigitOne) {
+                bullsCount++;
+            }
+
+            System.out.println("There are " + bullsCount + " bulls in your number");
+            //adding to cow counter
+
+            //checking if the 4th digit is matching with any of the rest and adds to cows' counter
+            if (userInputDigitFour == guessDigitThree) {
+                cowsCount++;
+            }
+
+            if (userInputDigitFour == guessDigitTwo) {
+                cowsCount++;
+            }
+
+            if (userInputDigitFour == guessDigitOne) {
+                cowsCount++;
+            }
+            //checking if the 3th digit is matching with any of the rest and adds to cows' counter
+            if (userInputDigitThree == guessDigitFour) {
+                cowsCount++;
+            }
+
+            if (userInputDigitThree == guessDigitTwo) {
+                cowsCount++;
+            }
+
+            if (userInputDigitThree == guessDigitOne) {
+                cowsCount++;
+            }
+            //checking if the 2nd digit is matching with any of the rest and adds to cows' counter
+            if (userInputDigitTwo == guessDigitFour) {
+                cowsCount++;
+            }
+
+            if (userInputDigitTwo == guessDigitThree) {
+                cowsCount++;
+            }
+
+            if (userInputDigitTwo == guessDigitOne) {
+                cowsCount++;
+            }
+            //checking if the 1st digit is matching with any of the rest and adds to cows' counter
+            if (userInputDigitOne == guessDigitFour) {
+                cowsCount++;
+            }
+
+            if (userInputDigitOne == guessDigitThree) {
+                cowsCount++;
+            }
+
+            if (userInputDigitOne == guessDigitTwo) {
+                cowsCount++;
+            }
+
+            System.out.println("There are " + cowsCount + " cows");
+            cowsCount = 0;
+            bullsCount = 0;
+
+            if(userInput == numberToGuess){
+                isGameRunning = false;
+            }
+
+        }
+
     }
 
-    public static int GeneratesRandomNumber() {
+    //method that generates four digit number with no repetitive digits
+    public static int generatesRandomNumber() {
         Random rand = new Random();
         int digitOne = rand.nextInt(9) + 1;
         int digitTwo = 0;
@@ -14,15 +139,15 @@ public class CowsAndBullsTest {
         int digitFour = 0;
 
         do {
-            digitTwo = rand.nextInt(9) + 1;
+            digitTwo = rand.nextInt(10);
         } while (digitTwo == digitOne);
 
         do {
-            digitThree = rand.nextInt(9) + 1;
+            digitThree = rand.nextInt(10);
         } while (digitThree == digitOne || digitThree == digitTwo);
 
         do {
-            digitFour = rand.nextInt(9) + 1;
+            digitFour = rand.nextInt(10);
         } while (digitFour == digitOne || digitFour == digitTwo || digitFour == digitThree);
 
         String digitOneStr = Integer.toString(digitOne);
@@ -37,4 +162,3 @@ public class CowsAndBullsTest {
         return fourDigitNumber;
     }
 }
-
