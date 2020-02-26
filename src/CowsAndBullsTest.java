a5d8eb62ab239e62368ae01439bea09a87a869b5
Ptr <petarman96@abv.bg>
2020-02-26 12:04:07 +0200

    added a lot of new methods like: countsBulls(), countsCows(), takesEachDigit() (works only for four digit numbers), createsOnePlayerGame(), getsUserInput etc. I forgot to mention last time but now the generatesRandomNumber() method can generate zero's except for the first digit. I am planning to add loops to replace the if's in countsCows() and countsBulls() methods later.

--- src/CowsAndBullsTest.java
+++ src/CowsAndBullsTest.java
@@ -3,133 +3,26 @@ import java.util.Scanner;
 
 public class CowsAndBullsTest {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int numberToGuess = generatesRandomNumber();
-
-
-        int bullsCount = 0;
-        int cowsCount = 0;
-
-        boolean isGameRunning = true;
-
-        while (isGameRunning) {
-            System.out.print(numberToGuess + " Enter 4 digit number with different digits: ");
-            int userInput = input.nextInt();
-
-
-            //taking each digit of the guess number
-            int tempGuessNumber = numberToGuess;
-
-            int guessDigitOne = tempGuessNumber % 10;
-            tempGuessNumber = tempGuessNumber / 10;
-
-            int guessDigitTwo = tempGuessNumber % 10;
-            tempGuessNumber = tempGuessNumber / 10;
-
-            int guessDigitThree = tempGuessNumber % 10;
-            tempGuessNumber = tempGuessNumber / 10;
-
-            int guessDigitFour = tempGuessNumber % 10;
-            tempGuessNumber = tempGuessNumber / 10;
-
-
-            //taking each digit of the user input number
-            int tempUserInputNumber = userInput;
-
-            int userInputDigitOne = tempUserInputNumber % 10;
-            tempUserInputNumber = tempUserInputNumber / 10;
-
-            int userInputDigitTwo = tempUserInputNumber % 10;
-            tempUserInputNumber = tempUserInputNumber / 10;
-
-            int userInputDigitThree = tempUserInputNumber % 10;
-            tempUserInputNumber = tempUserInputNumber / 10;
-
-            int userInputDigitFour = tempUserInputNumber % 10;
-            tempUserInputNumber = tempUserInputNumber / 10;
-
-
-            //adding to bull counter
-            if (userInputDigitFour == guessDigitFour) {
-                bullsCount++;
-            }
-
-            if (userInputDigitThree == guessDigitThree) {
-                bullsCount++;
-            }
-
-            if (userInputDigitTwo == guessDigitTwo) {
-                bullsCount++;
-            }
-
-            if (userInputDigitOne == guessDigitOne) {
-                bullsCount++;
-            }
-
-            System.out.println("There are " + bullsCount + " bulls in your number");
-            //adding to cow counter
-
-            //checking if the 4th digit is matching with any of the rest and adds to cows' counter
-            if (userInputDigitFour == guessDigitThree) {
-                cowsCount++;
-            }
-
-            if (userInputDigitFour == guessDigitTwo) {
-                cowsCount++;
-            }
-
-            if (userInputDigitFour == guessDigitOne) {
-                cowsCount++;
-            }
-            //checking if the 3th digit is matching with any of the rest and adds to cows' counter
-            if (userInputDigitThree == guessDigitFour) {
-                cowsCount++;
-            }
-
-            if (userInputDigitThree == guessDigitTwo) {
-                cowsCount++;
-            }
-
-            if (userInputDigitThree == guessDigitOne) {
-                cowsCount++;
-            }
-            //checking if the 2nd digit is matching with any of the rest and adds to cows' counter
-            if (userInputDigitTwo == guessDigitFour) {
-                cowsCount++;
-            }
-
-            if (userInputDigitTwo == guessDigitThree) {
-                cowsCount++;
-            }
-
-            if (userInputDigitTwo == guessDigitOne) {
-                cowsCount++;
-            }
-            //checking if the 1st digit is matching with any of the rest and adds to cows' counter
-            if (userInputDigitOne == guessDigitFour) {
-                cowsCount++;
-            }
-
-            if (userInputDigitOne == guessDigitThree) {
-                cowsCount++;
-            }
-
-            if (userInputDigitOne == guessDigitTwo) {
-                cowsCount++;
-            }
-
-            System.out.println("There are " + cowsCount + " cows");
-            cowsCount = 0;
-            bullsCount = 0;
-
-            if(userInput == numberToGuess){
-                isGameRunning = false;
-            }
-
-        }
-
+        createsOnePlayerGame();
     }
 
+    public static void createsOnePlayerGame(){
+        boolean isGameOn = true;
+        int computerNumber = generatesRandomNumber();
+
+        while(isGameOn){
+            System.out.println(computerNumber);
+            int player = getsPlayerInput();
+
+            countsCows(player, computerNumber);
+            System.out.println("There are " + countsBulls(player, computerNumber) + " bulls in your number.");
+
+            if(countsBulls(player, computerNumber) == 4){
+                isGameOn = false;
+            }
+        }
+        System.out.println("Congratulations you guessed the number!");
+    }
     //method that generates four digit number with no repetitive digits
     public static int generatesRandomNumber() {
         Random rand = new Random();
@@ -161,4 +54,144 @@ public class CowsAndBullsTest {
 
         return fourDigitNumber;
     }
+
+    public static int takesEachDigit(int number, int digit){
+        int numberToReturn = 0;
+
+        int tempNumber = number;
+
+        int digitOne = tempNumber % 10;
+        tempNumber = tempNumber/10;
+
+        int digitTwo = tempNumber % 10;
+        tempNumber = tempNumber/10;
+
+        int digitThree = tempNumber % 10;
+        tempNumber = tempNumber/10;
+
+        int digitFour = tempNumber % 10;
+        tempNumber = tempNumber/10;
+
+        if(digit == 4) {
+            numberToReturn = digitOne;
+        } else if(digit == 3) {
+            numberToReturn = digitTwo;
+        } else if(digit == 2) {
+            numberToReturn = digitThree;
+        } else if(digit == 1){
+            numberToReturn = digitFour;
+        }
+
+        return numberToReturn;
+    }
+
+    public static int countsBulls(int numberOne, int numberTwo){
+        int bulls = 0;
+        if ((takesEachDigit(numberOne, 1)) == (takesEachDigit(numberTwo, 1))) {
+            bulls++;
+        }
+
+        if ((takesEachDigit(numberOne, 2)) == (takesEachDigit(numberTwo, 2))) {
+            bulls++;
+        }
+
+        if ((takesEachDigit(numberOne, 3)) == (takesEachDigit(numberTwo, 3))) {
+            bulls++;
+        }
+
+        if ((takesEachDigit(numberOne, 4)) == (takesEachDigit(numberTwo, 4))) {
+            bulls++;
+        }
+
+        return bulls;
+    }
+
+    public static void countsCows(int numberOne, int numberTwo){
+        int cows = 0;
+        if (takesEachDigit(numberOne, 4) == takesEachDigit(numberTwo, 3)) {
+            if(takesEachDigit(numberOne, 3) != takesEachDigit(numberTwo, 3)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 4) == takesEachDigit(numberTwo, 2)) {
+            if(takesEachDigit(numberOne, 2) != takesEachDigit(numberTwo, 2)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 4) == takesEachDigit(numberTwo, 1)) {
+            if(takesEachDigit(numberOne, 1) != takesEachDigit(numberTwo, 1)) {
+                cows++;
+            }
+        }
+        //checking if the 3th digit is matching with any of the rest and adds to cows' counter
+        if (takesEachDigit(numberOne, 3) == takesEachDigit(numberTwo, 4)) {
+            if (takesEachDigit(numberOne, 4) != takesEachDigit(numberTwo, 4)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 3) == takesEachDigit(numberTwo, 2)) {
+            if (takesEachDigit(numberOne, 2) != takesEachDigit(numberTwo, 2)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 3) == takesEachDigit(numberTwo, 1)) {
+            if (takesEachDigit(numberOne, 1) != takesEachDigit(numberTwo, 1)) {
+                cows++;
+            }
+        }
+        //checking if the 2nd digit is matching with any of the rest and adds to cows' counter
+        if (takesEachDigit(numberOne, 2) == takesEachDigit(numberTwo, 4)) {
+            if (takesEachDigit(numberOne, 4) != takesEachDigit(numberTwo, 4)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 2) == takesEachDigit(numberTwo, 3)) {
+            if (takesEachDigit(numberOne, 3) != takesEachDigit(numberTwo, 3)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 2) == takesEachDigit(numberTwo, 1)) {
+            if (takesEachDigit(numberOne, 1) != takesEachDigit(numberTwo, 1)) {
+                cows++;
+            }
+        }
+        //checking if the 1st digit is matching with any of the rest and adds to cows' counter
+        if (takesEachDigit(numberOne, 1) == takesEachDigit(numberTwo, 4)) {
+            if (takesEachDigit(numberOne, 4) != takesEachDigit(numberTwo, 4)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 1) == takesEachDigit(numberTwo, 3)) {
+            if (takesEachDigit(numberOne, 3) != takesEachDigit(numberTwo, 3)) {
+                cows++;
+            }
+        }
+
+        if (takesEachDigit(numberOne, 1) == takesEachDigit(numberTwo, 2)) {
+            if (takesEachDigit(numberOne, 2) != takesEachDigit(numberTwo, 2)) {
+                cows++;
+            }
+        }
+        System.out.println("There are " + cows +" cows in your number.");
+
+    }
+
+    public static int getsPlayerInput(){
+        Scanner input = new Scanner(System.in);
+        int userInput = input.nextInt();
+
+        return userInput;
+    }
+
+
+
+
+
 }
